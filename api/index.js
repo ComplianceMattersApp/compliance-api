110e5804b309dac9fe26d7d0461bd311d07c8d47
ComplianceMattersApp <eddie@compliancemattersca.com>
2025-08-09 08:51:49 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://app.compliancemattersca.com");
-res.setHeader("Vary", "Origin");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    // Preflight request
+    return res.status(200).end();
+  }
+
   res.status(200).json({ service: "Compliance API", ok: true });
 }
