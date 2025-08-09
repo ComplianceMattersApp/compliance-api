35427499f28b2dd84d4461b852428f9bcd9d49ec
ComplianceMattersApp <eddie@compliancemattersca.com>
2025-08-09 09:12:18 -0700

    Update version.js

--- api/version.js
+++ api/version.js
@@ -1,7 +1,7 @@
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://app.compliancemattersca.com");
-res.setHeader("Vary", "Origin");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  
-  res.status(200).json({ version: "v0.1", env: process.env.VERCEL_ENV || "production" });
+import { withCors } from '../lib/cors';
+
+function handler(req, res) {
+  res.status(200).json({ version: 'v0.1', env: 'production' });
 }
+
+export default withCors(handler);
