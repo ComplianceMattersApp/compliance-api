308f4d22d4aebcb306277980c50c7fa043c48119
ComplianceMattersApp <eddie@compliancemattersca.com>
2025-08-09 08:41:37 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://app.compliancemattersca.com");
+res.setHeader("Vary", "Origin");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
   res.status(200).json({ service: "Compliance API", ok: true });
 }
